6b05a378a8f967a6e35c73c32edfbea4ffe295f5
yunline <yunline@foxmail.com>
2024-10-21 17:24:52 +0800

    Dependencies: Add version requirement of python interpreter (py3.11+).

--- linisolv/__init__.py
+++ linisolv/__init__.py
@@ -1,3 +1,10 @@
+import sys
+
+if sys.version_info<(3,11):
+    raise RuntimeError("LiniSolv requires Python version >= 3.11")
+
+del sys # clean the namespace
+
 from .linsolv import *
 from .parser import parse_circuit
 
